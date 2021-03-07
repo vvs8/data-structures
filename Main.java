ca50a0eac0b63b452cbce1c228b293c61e26944f
Vadym Shakhraichuk <64581387+vvs8@users.noreply.github.com>
2021-03-06 22:37:44 -0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,22 @@
+class Main {
+
+  public static void main(String[] args) {
+    
+    //ArrayStack
+    ArrayStack<Integer> test = new ArrayStack<Integer>();
+    test.push(2);
+    test.push(3);
+    test.push(4);
+    System.out.println(test.pop());
+    System.out.println(test.pop());
+    System.out.println(test.pop());
+    //System.out.println(test.pop());
+    
+    
+    
+    //List-Based Queue
+    
+    
+    
+  }
+}
